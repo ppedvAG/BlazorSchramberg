d3b8fda462df468e8fab8fb677291e640af35ed2
HannesPreishuber <hannesp@ppedv.de>
2021-09-02 16:33:11 +0200

    Httpclient und JSON Deserialisierung

---
+++ BlazorSchramberg/Pages/modul4/Todos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSchramberg.Pages.modul4
+{
+    public class Todos
+    {
+   
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+    }
+
+}
