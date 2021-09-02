2994b68669048ad7ca5c4655c70250d6229192c3
HannesPreishuber <hannesp@ppedv.de>
2021-09-02 10:47:07 +0200

    Components Basics und Codebehind

---
+++ BlazorSchramberg/Pages/modul1/Hannes2.razor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSchramberg.Pages.modul1
+{
+    public partial class Hannes2
+    {
+    }
+}
