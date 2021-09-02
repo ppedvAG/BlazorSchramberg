e99620a61e6d71c10c076e7b0058eee77ed5520a
HannesPreishuber <hannesp@ppedv.de>
2021-09-02 11:58:36 +0200

    Razor Routing (nicht Blazor)

---
+++ BlazorSchramberg/Pages/modul2/Hannes.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorSchramberg.Pages.modul2
+{
+    public class HannesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
