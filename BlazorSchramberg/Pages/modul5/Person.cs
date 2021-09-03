98bd824330aad08997b6a08df2f37000e286e9f2
HannesPreishuber <hannesp@ppedv.de>
2021-09-03 10:03:50 +0200

    einfaches Formular mit Validierung

---
+++ BlazorSchramberg/Pages/modul5/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSchramberg.Pages.modul5
+{
+    public class Person
+    {
+        public int ID { get; set; }
+
+        [Range(100,200,ErrorMessage ="Keine Achterbahn")]
+        [Required(ErrorMessage = "da muss was rein")]
+        public int Gross { get; set; }
+        [StringLength(10,ErrorMessage ="max 10")]
+        [Required(ErrorMessage ="da muss was rein")]
+        public string Name { get; set; }
+        public DateTime GebDatum { get; set; }
+    }
+}
