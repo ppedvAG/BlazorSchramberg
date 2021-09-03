8e50a0791486b5a5ccb12580185de2d9e57940d6
HannesPreishuber <hannesp@ppedv.de>
2021-09-03 10:21:58 +0200

    HBootstrap Error Fix    FieldCssClassProvider

---
+++ BlazorSchramberg/Pages/modul5/InputErrorfixBoostrapFieldClassCssProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorSchramberg.Pages.modul5
+{
+    public class InputErrorfixBoostrapFieldClassCssProvider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, 
+            in FieldIdentifier fieldIdentifier)
+        {
+            var isvalid=!editContext.GetValidationMessages(fieldIdentifier).Any();
+            return isvalid ? "is-valid" : "is-invalid";
+        }
+    }
+}
